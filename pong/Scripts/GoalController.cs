b82e9b6a94d581c27941dfbc646a120319e06e88
Will Hopper <whopper@puppetlabs.com>
2016-12-02 10:48:09 -0800

    Add Goal Controller

---
+++ pong/Scripts/GoalController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalController : MonoBehaviour {
+
+    public bool scored;
+
+    void Start () {
+        scored = false;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Ball") {
+            scored = true;
+        }
+    }
+}
